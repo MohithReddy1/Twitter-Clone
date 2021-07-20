e4c48d0f1233a6ea5499618d73b0b9c9226798c3
MohithReddy1 <vuyyuru.reddy.19cs@bmu.edu.in>
2021-07-20 10:19:19 +0530

    socket

--- public/js/clientScoket.js
+++ public/js/clientScoket.js
@@ -1,8 +1,8 @@
 let connected = false;
 
-//let socket = io("https://twitter9.herokuapp.com/")
+let socket = io("https://twitter9.herokuapp.com/")
 
-let socket = io("http://localhost:3002/")
+//let socket = io("http://localhost:3002/")
 socket.emit("setup", userLoggedIn);
 
 socket.on("connected", () => connected = true);
